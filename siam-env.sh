095f98966bf49dddb12b8a5911c42e9d561d60b2
Tom O'Reilly <oreilly@mbari.org>
2013-03-05 09:02:29 -0800

    Modified SIAM_CLASSPATH; removed javax.jar, fixed path to rbnb.jar

--- siam-env.sh
+++ siam-env.sh
@@ -10,7 +10,7 @@ alias gosiam="cd $SIAM_HOME"
 alias inf="export LOG4J_THRESHOLD=INFO"
 alias dbg="export LOG4J_THRESHOLD=DEBUG"
 
-export SIAM_CLASSPATH=$SIAM_HOME/classes:$SIAM_HOME/jars/log4j-1.2.13.jar:$SIAM_HOME/jars/javax.jar:$SIAM_HOME/jars/ssds-client-pub.jar:$SIAM_HOME/jars/jddac-common.jar:$SIAM_HOME/jars/jddac-probe-j2me.jar:$SIAM_HOME/jars/RXTXcomm.jar:/Applications/RBNB/bin/rbnb.jar:$SIAM_HOME/jars/puck.jar
+export SIAM_CLASSPATH=$SIAM_HOME/classes:$SIAM_HOME/jars/log4j-1.2.13.jar:$SIAM_HOME/jars/ssds-client-pub.jar:$SIAM_HOME/jars/jddac-common.jar:$SIAM_HOME/jars/jddac-probe-j2me.jar:$SIAM_HOME/jars/RXTXcomm.jar:$SIAM_HOME/jars/rbnb.jar:$SIAM_HOME/jars/puck.jar
 
 
 export PATH=$SIAM_HOME/utils:$SIAM_HOME/shoreUtils:$PATH
